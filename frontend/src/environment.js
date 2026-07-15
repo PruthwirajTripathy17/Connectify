3a25d7b61d6189aae554b8449925e6ca0d690c54
Pruthwiraj Tripathy <pruthwirajtripathy2@gmail.com>
2026-07-15 23:01:54 +0530

    add automatic environment detection for local and prod

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,16 @@
-let IS_PROD=false;
-const server = IS_PROD ?
-"https://connectifybackend-qpd2.onrender.com":
-`http://${window.location.hostname}:8000`;
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '[::1]' ||
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  ) ||
+  window.location.hostname.startsWith('192.168.') ||
+  window.location.hostname.startsWith('10.') ||
+  window.location.hostname.startsWith('172.')
+);
+
+const server = isLocalhost ?
+`http://${window.location.hostname}:8000`:
+"https://connectifybackend-qpd2.onrender.com";
 
 export default server;
